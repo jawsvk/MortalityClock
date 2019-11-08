2ddf44fd6ae71fa40ea58d14e46caecffe68244a
Jawen Voon <weinchester@gmail.com>
2019-11-08 16:46:54 +0800

    Bind binding data with form

---
+++ MortalityClock/Data/DateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortalityClock.Data
+{
+    public class DateModel
+    {
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public DateTime DeathDate{ get; set; }
+
+    }
+}
