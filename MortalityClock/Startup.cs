5c5eee62dd1dc7e2b81de4fb145f92836d063e0d
Jawen Voon <weinchester@gmail.com>
2019-11-08 14:40:38 +0800

    remove unnecessary components

--- MortalityClock/Startup.cs
+++ MortalityClock/Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MortalityClock.Data;
-
 namespace MortalityClock
 {
     public class Startup
@@ -28,7 +26,6 @@ namespace MortalityClock
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
