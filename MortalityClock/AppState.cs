bf358f71fd3e97de88b3efd317541e5bf0912933
Jawen Voon <weinchester@gmail.com>
2019-11-11 16:34:41 +0800

    add AppState class for state management

---
+++ MortalityClock/AppState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortalityClock
+{
+    public class AppState
+    {
+        public DateTime BirthDate { get; private set;}
+        public int Duration;
+        public event Action OnChange;
+
+        public void SetBirthDate(DateTime date)
+        {
+            BirthDate = date;
+            NotifyStateChanged();
+
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        
+     
+    }
+}
